4ae41ef7c924598ee1d914ad8f951a2698928285
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-09-03 12:33:06 +0530

    kick off 0.2.0 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-viz "0.1.0"
+(defproject lein-viz "0.2.0-SNAPSHOT"
   :description "Leiningen plugin to visualize graph and tree data"
   :url "https://github.com/kumarshantanu/lein-viz"
   :license {:name "Eclipse Public License"
